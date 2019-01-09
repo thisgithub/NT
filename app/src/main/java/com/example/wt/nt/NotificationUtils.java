cf1633c33552b3d3048baebf641119eabf774d4e
thisgithub <15165616852@163.com>
2019-01-09 10:56:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wt/nt/NotificationUtils.java
@@ -0,0 +1,60 @@
+package com.example.wt.nt;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationUtils extends ContextWrapper {
+
+    private NotificationManager manager;
+    public static final String id = "channel_1";
+    public static final String name = "channel_name_1";
+
+    public NotificationUtils(Context base) {
+        super(base);
+    }
+
+    public void createNotificationChannel() {
+        NotificationChannel channel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH);
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if (manager == null) {
+            manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        }
+        return manager;
+    }
+
+    public Notification.Builder getChannelNotification(String title, String content) {
+        return new Notification.Builder(getApplicationContext(), id)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true);
+    }
+
+    public NotificationCompat.Builder getNotification_25(String title, String content) {
+        return new NotificationCompat.Builder(getApplicationContext())
+                .setContentTitle(title)
+                .setContentText(content)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true);
+    }
+
+    public void sendNotification(String title, String content) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel();
+            Notification notification = getChannelNotification(title, content).build();
+            getManager().notify(1, notification);
+        } else {
+            Notification notification = getNotification_25(title, content).build();
+            getManager().notify(1, notification);
+        }
+    }
+}
